c3aaffddcb301a4249d525d0247804e78d6af97d
Zach <zackify@gmail.com>
2014-02-12 16:54:51 -0800

    removed the need for jquery and added nested elements, readme update coming soon

--- src/forms.js
+++ src/forms.js
@@ -3,20 +3,19 @@
 var formjs = React.createClass({
   updateValues: function(element){
     var currentValues = this.state.values;
-    currentValues[element.id] = {name: element.name, value: element.value};
+    if(element.childId){
+      if(!currentValues[element.id]) currentValues[element.id] = [];
+      currentValues[element.id][element.childId] = {name: element.name, value: element.value};
+    }
+    else{
+      currentValues[element.id] = {name: element.name, value: element.value};
+    }
     this.setState({values: currentValues});
+    this.props.currentState(this.state.values);
     return false;
   },
   handleSubmit: function() {
-    $.ajax({
-      url: this.state.data.href,
-      dataType: 'json',
-      data: this.state.values,
-      type: this.state.data.method,
-      success: function(data) {
-        //this.setState({data: data});
-      }.bind(this)
-    });
+    this.props.submitState(this.state.values);
     return false;
   },
   getInitialState: function(){
@@ -30,39 +29,49 @@ var formjs = React.createClass({
     //console.log(this.state.properties);
     var elements = _.map(this.state.properties, function (property,name) {
       id++;
-      if(property.type == "string"){
-        var fieldType = "text";
-        if (property.format) fieldType = property.format;
-        if (!property.title) property.title = name;
-
-        return <generateInputField 
-        type         = {fieldType} 
-        label        = {property.title} 
-        name         = {name} 
-        placeholder  = {property['ux-placeholder']} 
-        value        = {property.value}
-        required     = {property.required}
-        updateValues = {updateValues}
-        id           = {id} />;
+      if(property.type == "array"){
+        if(property.items.properties){
+          var childId = 0;
+          var childElements = _.map(property.items.properties, function (property,name) {
+            childId++;
+            var fieldType = "text";
+            return <generateInputField 
+            type         = {fieldType} 
+            label        = {property.title} 
+            name         = {name} 
+            placeholder  = {property['ux-placeholder']} 
+            value        = {property.value}
+            required     = {property.required}
+            description  = {property.description}
+            updateValues = {updateValues}
+            child        = "true"
+            id           = {id}
+            childId      = {childId} />;
+          });
+          return childElements;
+        }
       }
-      if(property.type == "number"){
-        var fieldType = "number";
-        if (property.format) fieldType = property.format; // if you want to use a range field
-        if (!property.title) property.title = name;
-        
-        return <generateInputField 
-        type         = {fieldType} 
-        label        = {property.title} 
-        name         = {name} 
-        placeholder  = {property['ux-placeholder']} 
-        value        = {property.value}
-        required     = {property.required}
-        updateValues = {updateValues}
-        id           = {id} />;
+      else{
+        if(property.type == "string" || property.type == "number"){
+          var fieldType = "text";
+          if (property.type == "number") fieldType = "number";
+          if (property.format) fieldType = property.format;
+          if (!property.title) property.title = name;
+          return <generateInputField 
+          type         = {fieldType} 
+          label        = {property.title} 
+          name         = {name} 
+          placeholder  = {property['ux-placeholder']} 
+          value        = {property.value}
+          required     = {property.required}
+          description  = {property.description}
+          updateValues = {updateValues}
+          id           = {id} />;
+        }
       }
     });
     return(
-      <form onSubmit={this.handleSubmit}>
+      <form encType="multipart/form-data" onSubmit={this.handleSubmit}>
         {elements}
         <input type="submit" value={this.state.data['ux-submit-text']} />
       </form>
@@ -79,12 +88,13 @@ var generateInputField = React.createClass({
   handleChange: function(e) {
     var name = this.props.name;
     var value = e.target.value;
-    this.props.updateValues({id: this.props.id, name: name, value: value});
+    this.props.updateValues({id: this.props.id, name: name, value: value,child: this.props.child,childId: this.props.childId});
     this.setState({value: value});
   },
   render: function(){
     return(
       <div className="element textfield">
+      <p dangerouslySetInnerHTML={{__html: this.props.description}} />
       <label>{this.props.label}</label>
       <input type = {this.props.type}
       placeholder = {this.props.placeholder}
@@ -96,6 +106,6 @@ var generateInputField = React.createClass({
   }
 });
 React.renderComponent(
-  <formjs data={json} />,
+  <formjs data={json} submitState={submitState} currentState={currentState} />,
   document.body
 );
